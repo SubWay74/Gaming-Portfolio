6548464ae4effa612897ea2c63e942e626f94e36
SubWay <sub74741@gmail.com>
2023-02-10 17:16:28 +0200

    changing paths, photoshop card's width and height, removing console logs

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ function cursorMove(e) {
 }
 window.addEventListener("mousemove", cursorMove)
 
-const music = new Audio('/Rebirth.mp3');
+const music = new Audio('../audio/Rebirth.mp3');
 const playBtn = document.getElementById("playbtn");
 const musicLine = document.querySelector(".musics p");
 music.setAttribute("preload", "none");
@@ -61,43 +61,44 @@ anime({
 let isDown = false;
 let startX;
 let scrollLeft;
-const slider = document.querySelectorAll('.items');
+const sliders = document.querySelectorAll('.items');
 
 
-slider.forEach( (j) => {
+sliders.forEach( (slider) => {
     const end = () => {
         isDown = false;
     }
     
     const start = (e) => {
         isDown = true;
-        startX = e.pageX || e.touches[0].pageX - j.offsetLeft; 
-        scrollLeft = j.scrollLeft;	
+        startX = e.pageX || e.touches[0].pageX - slider.offsetLeft; 
+        scrollLeft = slider.scrollLeft;	
     }
     
     const move = (e) => {
         if(!isDown) return;
         e.preventDefault();
-        const x = e.pageX || e.touches[0].pageX - j.offsetLeft;
+        const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
         const dist = (x - startX);
-        j.scrollLeft = scrollLeft - dist;
+        slider.scrollLeft = scrollLeft - dist;
         // slider.scrollLeft -= dist;
     }
+
     
     (() => {
-        j.addEventListener('mousedown', start);
+        slider.addEventListener('mousedown', start);
         
-        j.addEventListener('touchstart', start);
+        slider.addEventListener('touchstart', start);
     
-        j.addEventListener('mousemove', move);
+        slider.addEventListener('mousemove', move);
         
-        j.addEventListener('touchmove', move);
+        slider.addEventListener('touchmove', move);
     
-        j.addEventListener('mouseleave', end);
+        slider.addEventListener('mouseleave', end);
         
-        j.addEventListener('mouseup', end);
+        slider.addEventListener('mouseup', end);
         
-        j.addEventListener('touchend', end);
+        slider.addEventListener('touchend', end);
     })();
 })
 
@@ -107,12 +108,7 @@ const footerImg = document.querySelector(".img-footer");
 const footerHeader = document.querySelector(".h1-footer");
 const footerUl = document.querySelector(".ul-footer");
 
-console.log(footerImg)
-console.log(footerHeader)
-console.log(footerUl)
-
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY)
     if(window.scrollY >= 1300) {
         footerImg.classList.add("active");
         footerHeader.classList.add("active");
